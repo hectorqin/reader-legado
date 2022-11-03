26a72af36dbd4efd1aa0ec1616fa0349da5bd476
hectorqin <hectorqin@163.com>
2022-11-03 17:30:03 +0800

    新增RSA加解密方法

--- src/main/java/io/legado/app/utils/EncoderUtils.kt
+++ src/main/java/io/legado/app/utils/EncoderUtils.kt
@@ -5,6 +5,11 @@ import java.security.spec.AlgorithmParameterSpec
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
+import java.io.ByteArrayOutputStream
+import java.security.KeyPairGenerator
+import java.security.KeyPair
+import java.security.PrivateKey
+import java.security.PublicKey
 
 @Suppress("unused")
 object EncoderUtils {
@@ -338,4 +343,216 @@ object EncoderUtils {
         return symmetricTemplate(data, key, "DESede", transformation, iv, false)
     }
 
+    /**
+     * 非对称加密RSA加密和解密
+     */
+    /**
+     * 私钥加密
+     */
+    fun encryptByPrivateKey(input: String, privateKey: PrivateKey): String {
+        /********************非对称加/解密三部曲**********************/
+        //1.创建cipher对象
+        val cipher = Cipher.getInstance("RSA")
+        //2.初始化cipher
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey)
+        //3.加密/解密
+        val encrypt = cipher.doFinal(input.toByteArray())
+        return Base64.encodeToString(encrypt, Base64.NO_WRAP)
+    }
+
+    /**
+     * 公钥解密
+     */
+    fun decryptByPublicKey(input: String, publicKey: PublicKey): String {
+        val decode = Base64.decode(input, Base64.NO_WRAP)
+        /********************非对称加/解密三部曲**********************/
+        //1.创建cipher对象
+        val cipher = Cipher.getInstance("RSA")
+        //2.初始化cipher
+        cipher.init(Cipher.DECRYPT_MODE, publicKey)
+        //3.加密/解密
+        val encrypt = cipher.doFinal(decode)
+        return String(encrypt)
+    }
+
+    /**
+     * 公钥加密
+     */
+    fun encryptByPublicKey(input: String, publicKey: PublicKey): String {
+        /********************非对称加/解密三部曲**********************/
+        //1.创建cipher对象
+        val cipher = Cipher.getInstance("RSA")
+        //2.初始化cipher
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+        //3.加密/解密
+        val encrypt = cipher.doFinal(input.toByteArray())
+        return Base64.encodeToString(encrypt, Base64.NO_WRAP)
+    }
+
+    /**
+     * 私钥解密
+     */
+    fun decryptByPrivateKey(input: String, privateKey: PrivateKey): String {
+        val decode = Base64.decode(input, Base64.NO_WRAP)
+        /********************非对称加/解密三部曲**********************/
+        //1.创建cipher对象
+        val cipher = Cipher.getInstance("RSA")
+        //2.初始化cipher
+        cipher.init(Cipher.DECRYPT_MODE, privateKey)
+        //3.加密/解密
+        val encrypt = cipher.doFinal(decode)
+        return String(encrypt)
+    }
+
+    /**
+     * 私钥分段加密
+     */
+    fun encryptSegmentByPrivateKey(input: String, privateKey: PrivateKey, keySize: Int = 2048): String {
+        val byteArray = input.toByteArray()
+
+        var temp:ByteArray
+        var offset = 0 //当前偏移的位置
+        val MAX_ENCRYPT_BLOCK = keySize / 8 - 11
+
+        val bos = ByteArrayOutputStream()
+
+        /********************非对称加/解密三部曲**********************/
+        //1.创建cipher对象
+        val cipher = Cipher.getInstance("RSA")
+        //2.初始化cipher
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey)
+        //3.加密：分段加密
+        while (byteArray.size - offset > 0) { //没有加密完
+            //每次最大加密 MAX_ENCRYPT_BLOCK 个字节
+            if (byteArray.size - offset >= MAX_ENCRYPT_BLOCK) {
+                //剩余部分大于 MAX_ENCRYPT_BLOCK, 加密完整MAX_ENCRYPT_BLOCK
+                temp  = cipher.doFinal(byteArray, offset, MAX_ENCRYPT_BLOCK)
+                offset += MAX_ENCRYPT_BLOCK
+            } else {
+                //加密最后一块
+                temp  = cipher.doFinal(byteArray, offset, byteArray.size - offset)
+                offset = byteArray.size
+            }
+            //存储到临时缓冲区
+            bos.write(temp)
+        }
+        bos.close()
+        return Base64.encodeToString(bos.toByteArray(), Base64.NO_WRAP)
+    }
+
+    /**
+     * 公钥分段解密
+     */
+    fun decryptSegmentByPublicKey(input: String, publicKey: PublicKey, keySize: Int = 2048): String? {
+        val byteArray = Base64.decode(input, Base64.NO_WRAP)
+
+        var temp:ByteArray
+        var offset = 0 //当前偏移的位置
+        val MAX_DECRYPT_BLOCK = keySize / 8
+
+        val bos = ByteArrayOutputStream()
+
+        /********************非对称加/解密三部曲**********************/
+        //1.创建cipher对象
+        val cipher = Cipher.getInstance("RSA")
+        //2.初始化cipher
+        cipher.init(Cipher.DECRYPT_MODE, publicKey)
+        //3.解密：分段解密
+        while (byteArray.size - offset > 0) { //没有解密完
+            //每次最大解密 MAX_DECRYPT_BLOCK 个字节
+            if (byteArray.size - offset >= MAX_DECRYPT_BLOCK) {
+                //剩余部分大于 MAX_DECRYPT_BLOCK, 解密完整 MAX_DECRYPT_BLOCK
+                temp  = cipher.doFinal(byteArray, offset, MAX_DECRYPT_BLOCK)
+                offset += MAX_DECRYPT_BLOCK
+            } else {
+                //解密最后一块
+                temp  = cipher.doFinal(byteArray, offset, byteArray.size - offset)
+                offset = byteArray.size
+            }
+            //存储到临时缓冲区
+            bos.write(temp)
+        }
+        bos.close()
+
+        return String(bos.toByteArray())
+    }
+
+    /**
+     * 公钥分段加密
+     */
+    fun encryptSegmentByPublicKey(input: String, publicKey: PublicKey, keySize: Int = 2048): String {
+        val byteArray = input.toByteArray()
+
+        var temp:ByteArray
+        var offset = 0 //当前偏移的位置
+        val MAX_ENCRYPT_BLOCK = keySize / 8 - 11
+
+        val bos = ByteArrayOutputStream()
+
+        /********************非对称加/解密三部曲**********************/
+        //1.创建cipher对象
+        val cipher = Cipher.getInstance("RSA")
+        //2.初始化cipher
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+        //3.加密：分段加密
+        while (byteArray.size - offset > 0) { //没有加密完
+            //每次最大加密 MAX_ENCRYPT_BLOCK 个字节
+            if (byteArray.size - offset >= MAX_ENCRYPT_BLOCK) {
+                //剩余部分大于 MAX_ENCRYPT_BLOCK, 加密完整MAX_ENCRYPT_BLOCK
+                temp  = cipher.doFinal(byteArray, offset, MAX_ENCRYPT_BLOCK)
+                offset += MAX_ENCRYPT_BLOCK
+            } else {
+                //加密最后一块
+                temp  = cipher.doFinal(byteArray, offset, byteArray.size - offset)
+                offset = byteArray.size
+            }
+            //存储到临时缓冲区
+            bos.write(temp)
+        }
+        bos.close()
+
+        return Base64.encodeToString(bos.toByteArray(), Base64.NO_WRAP)
+    }
+
+    /**
+     * 私钥分段解密
+     */
+    fun decryptSegmentByPrivateKey(input: String, privateKey: PrivateKey, keySize: Int = 2048): String? {
+        val byteArray = Base64.decode(input, Base64.NO_WRAP)
+
+        var temp:ByteArray
+        var offset = 0 //当前偏移的位置
+        val MAX_DECRYPT_BLOCK = keySize / 8
+
+        val bos = ByteArrayOutputStream()
+
+        /********************非对称加/解密三部曲**********************/
+        //1.创建cipher对象
+        val cipher = Cipher.getInstance("RSA")
+        //2.初始化cipher
+        cipher.init(Cipher.DECRYPT_MODE, privateKey)
+        //3.解密：分段解密
+        while (byteArray.size - offset > 0) { //没有解密完
+            //每次最大解密 MAX_DECRYPT_BLOCK 个字节
+            if (byteArray.size - offset >= MAX_DECRYPT_BLOCK) {
+                //剩余部分大于 MAX_DECRYPT_BLOCK, 解密完整 MAX_DECRYPT_BLOCK
+                temp  = cipher.doFinal(byteArray, offset, MAX_DECRYPT_BLOCK)
+                offset += MAX_DECRYPT_BLOCK
+            } else {
+                //解密最后一块
+                temp  = cipher.doFinal(byteArray, offset, byteArray.size - offset)
+                offset = byteArray.size
+            }
+            //存储到临时缓冲区
+            bos.write(temp)
+        }
+        bos.close()
+
+        return String(bos.toByteArray())
+    }
+
+    fun generateKeys(): KeyPair {
+        val generator = KeyPairGenerator.getInstance("RSA")
+        return generator.genKeyPair()
+    }
 }
